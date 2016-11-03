cff4bf6ff61f4c2890b6776b1d3dd124694d118e
Raphael Ott <raphael.ott@best-gaming.com>
2016-11-03 13:31:35 +0100

    add hot-vs-cold functionality

--- src/app/hot-vs-cold/hot-vs-cold.component.ts
+++ src/app/hot-vs-cold/hot-vs-cold.component.ts
@@ -9,10 +9,34 @@ import * as Rx from 'rxjs';
 })
 export class HotVsColdComponent implements OnInit {
 
+  private currentValue1:number = 0;
+  private currentValue2:number = 0;
+
   constructor() { }
 
   ngOnInit() {
+    let coldStream = this.createObservable();
+
+    // coldStream.subscribe(value => this.currentValue1 = value);
+    // coldStream.subscribe(value => this.currentValue2 = value);
+
+    let hotStream = this.createObservable().publish();
+
+    hotStream.connect();
+
+    hotStream.subscribe(value => this.currentValue1 = value);
+    setTimeout(() => {
+      hotStream.subscribe(value => this.currentValue2 = value);
+    }, 3000);
     
+  }
+
+  private createObservable():Rx.Observable<number> {
+    return new Rx.Observable<number>(observer => {
+      setInterval(() => {
+        observer.next((Math.random() * (0 - 100) + 100).toFixed(0));
+      }, 1000);
+    });
   }
 
 }
